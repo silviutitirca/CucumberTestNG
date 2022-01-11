084122bc5f50549da40ad2bf3adbdb0cf8a73937
silviutitirca <s_titirca@yahoo.com>
2022-01-11 11:21:14 +0100

    Updated TestNGRunner to run all features

--- src/test/java/runners/TestNGRunner.java
+++ src/test/java/runners/TestNGRunner.java
@@ -7,11 +7,8 @@ import io.cucumber.testng.CucumberOptions;
  * @author Silviu_Titirca on 1/10/2022
  */
 @CucumberOptions(
-        features = "src/test/resources/Admin.feature",
+        features = "src/test/resources/",
         glue = {"stepdefinitions"},
-        dryRun = true,
         publish = true)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
-    public TestNGRunner() {
-    }
 }
